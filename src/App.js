009c1dd6596f49a9093bba596b0147885daa4415
Hozefa04 <98385176+Hozefa04@users.noreply.github.com>
2023-05-19 17:44:21 +0530

    added all changes from main react location app

--- src/App.js
+++ src/App.js
@@ -1,39 +1,94 @@
 import React, { useEffect } from "react";
-import { browserName } from "react-device-detect";
 import "./App.css";
-import appLogo from "./images/appicon.png";
+import { browserName } from "react-device-detect";
+// import appLogo from "./images/appicon.png";
+import appLogo from "./images/applogo.png";
+import appBg from "./images/bg.png";
+import appStoreLogo from "./images/appstore.png";
+import playStoreLogo from "./images/play.png";
+import appleLogo from "./images/apple-logo.jpeg";
+import androidLogo from "./images/android-logo.png";
 
 function App() {
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(function (position) {
-      console.log("Latitude is :", position.coords.latitude);
-      console.log("Longitude is :", position.coords.longitude);
+    navigator.geolocation.getCurrentPosition(
+      function (position) {
+        console.log("Latitude is :", position.coords.latitude);
+        console.log("Longitude is :", position.coords.longitude);
 
-      var windowName = "popup check";
-      var popUp = window.open(
-        "http://www.google.in",
-        windowName,
-        "width=1, height=1, left=0, top=0"
-      );
-      if (popUp == null || typeof popUp == "undefined") {
-        // alert(
-        //   'Please disable your pop-up blocker and click the "Open" link again.'
-        // );
-        openInNewTab(position.coords.latitude, position.coords.longitude, true);
-      } else {
-        popUp.close();
-        openInNewTab(
-          position.coords.latitude,
-          position.coords.longitude,
-          false
-        );
+        var popUp = checkPopup();
+        if (popUp == null || typeof popUp == "undefined") {
+          openInNewTab(
+            position.coords.latitude,
+            position.coords.longitude,
+            true
+          );
+        } else {
+          popUp.close();
+          openInNewTab(
+            position.coords.latitude,
+            position.coords.longitude,
+            false
+          );
+        }
+      },
+      function (error) {
+        var popUp = checkPopup();
+
+        if (popUp == null || typeof popUp == "undefined") {
+          openInNewTabNoLocation(true);
+        } else {
+          popUp.close();
+          openInNewTabNoLocation(false);
+        }
       }
-    });
+    );
   }, []);
+
+  const checkPopup = () => {
+    var windowName = "popup check";
+    return window.open(
+      "http://www.google.in",
+      windowName,
+      "width=1, height=1, left=0, top=0"
+    );
+  };
+
+  /**
+   * Determine the mobile operating system.
+   * This function returns one of 'iOS', 'Android', 'Windows Phone', or 'unknown'.
+   *
+   * @returns {String}
+   */
+  function getMobileOperatingSystem() {
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+    if (/windows phone/i.test(userAgent)) {
+      return "Windows Phone";
+    }
+
+    if (/android/i.test(userAgent)) {
+      return "Android";
+    }
+
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+      return "iOS";
+    }
+
+    return "unknown";
+  }
 
   const openInNewTab = (lat, long, popup) => {
     window.open(
-      `http://localhost:58795/?para1=${lat}&para2=${long}&browserName=${browserName}&isPopupBlocked=${popup}`,
+      `http://localhost:58795/?para1=${lat}&para2=${long}&browserName=${browserName}&isPopupBlocked=${popup}&isGlobal=false`,
+      "_self",
+      "noopener,noreferrer"
+    );
+  };
+
+  const openInNewTabNoLocation = (popup) => {
+    window.open(
+      `http://localhost:58795/?browserName=${browserName}&isPopupBlocked=${popup}&isGlobal=true`,
       "_self",
       "noopener,noreferrer"
     );
@@ -41,16 +96,48 @@ function App() {
 
   return (
     <div className="App">
-      <center>
-        <img src={appLogo} alt="new" />
-        {browserName === "Chrome" ? (
-          <p className="note">Please allow location access.</p>
+      <div className="background">
+        {/* <div className="storeLogo"> */}
+        <img className="bg" src={appBg} alt="store" />
+        {getMobileOperatingSystem() === "iOS" ? (
+          <a href="https://apps.apple.com/in/app/shotcaller-make-sports-picks/id1558033440">
+            <span className="allow-text">
+              Please allow location permission & popup to load games
+            </span>
+            <img className="logo" src={appLogo} alt="" />
+            <img className="store" src={appStoreLogo} alt="" />
+            <span className="download-text">
+              Download app for best experience
+            </span>
+            <div className="download-container">
+              <div className="inner-container">
+                <img className="apple-logo" src={appleLogo} alt="" />
+                <p className="store-text">Download iOS App</p>
+              </div>
+            </div>
+          </a>
         ) : (
-          <p className="note">
-            Please switch to chrome for a better experience.
-          </p>
+          <a href="https://play.google.com/store/apps/details?id=com.app.shotcaller">
+            <span className="allow-text">
+              Please allow location permission & popup to load games
+            </span>
+            {/*<span className="allow-text">Refresh browser & allow location permission & popups to load games</span>*/}
+            <img className="logo" src={appLogo} alt="" width={"20px"} />
+            <img className="store" src={playStoreLogo} alt="" />
+            <span className="download-text">
+              Download app for best experience
+            </span>
+            <div className="download-container">
+              <div className="inner-container">
+                <img className="android-logo" src={androidLogo} alt="" />
+                <p className="store-text">Download Android App</p>
+              </div>
+            </div>
+          </a>
         )}
-      </center>
+
+        {/* </div> */}
+      </div>
     </div>
   );
 }
